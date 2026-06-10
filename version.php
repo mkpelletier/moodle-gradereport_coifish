cb760a3a2d340bb42012570ce636ed5eeb2038f1
Mathieu Pelletier <mkpelletier@gmail.com>
2026-06-10 17:21:50 +0200

    Bump version to 2026061001 to force AMD module cache refresh on upgrade [skip ci]

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'gradereport_coifish';
-$plugin->version   = 2026061000;
+$plugin->version   = 2026061001;
 $plugin->release   = '2.7.0';
 $plugin->requires  = 2024110400; // Moodle 5.0+.
 $plugin->maturity  = MATURITY_STABLE;
